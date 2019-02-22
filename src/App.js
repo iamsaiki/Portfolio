b780699b94405efc2f53eb48243a94dd8530e9fd
Saiki K <sachin.winx007@gmail.com>
2019-02-22 16:26:16 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React,{ Component } from 'react';
 import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png';
 
 class App extends Component{
 
@@ -19,14 +21,15 @@ class App extends Component{
     render() {
         return (
             <div>
+                <img src={profile} alt='profile' className='profile'/>
                 <h1>Hello!</h1>
-                <p>My name is David. I'm a software developer.</p>
+                <p>My name is Sachin. I'm a software developer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
                 {this.state.displayBio ? (
                 <div>
                     <p>I live in San Francisco, and code every day.</p>
                     <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
-                    <p>Besides coding, I love music and ramen!</p>
+                    <p>Besides coding, I love music and football!</p>
                     <button onClick={this.toggleDisplayBio}>Show Less</button>
                 </div>
                 ) : (<div>
@@ -36,6 +39,8 @@ class App extends Component{
                 }
                 <hr/>
                 <Projects />
+                <hr/>
+                <SocialProfiles />
             </div>
         )
     }
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,28 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png'; 
+
+const SOCIAL_PROFILES = [
+        {
+            id: 1,
+            link: 'mailto:sachin.winx007@gmail.com',
+            image: emailIcon
+        },
+        {
+            id: 2,
+            link: 'https://github.com/iamsaiki',
+            image: githubIcon
+        },
+        {
+            id: 3,
+            link: 'https://www.linkedin.com/in/sachink01/',
+            image: linkedinIcon
+        },
+        {
+            id: 3,
+            link: 'https://www.twitter.com/sachin0x7/',
+            image: twitterIcon
+        }
+    ];
+    export default SOCIAL_PROFILES;
